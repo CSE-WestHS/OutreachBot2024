7caf7f933c8b67cc0f0c543232506683f6b90b33
6317DS1 <162824044+6317DS1@users.noreply.github.com>
2024-04-30 16:22:59 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,7 +148,8 @@ public class RobotContainer {
         Commands.run(
             () ->
                 drive.CurvatureDrive(
-                    ((controller.getRightTriggerAxis())), applyDeadband(-controller.getLeftX() / 2)),
+                    ((controller.getRightTriggerAxis())),
+                    applyDeadband(-controller.getLeftX() / 2)),
             drive));
     turret.setDefaultCommand(
         new GotoPosition(
