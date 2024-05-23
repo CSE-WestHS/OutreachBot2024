27428b53e57b4eefad806e9f4cfa7b08bda2e485
6317DS2 <162824217+6317DS2@users.noreply.github.com>
2024-05-23 18:40:04 -0500

    maintenance

--- src/main/java/frc/robot/subsystems/Limelight/LimelightAiming.java
+++ src/main/java/frc/robot/subsystems/Limelight/LimelightAiming.java
@@ -19,79 +19,9 @@ public class LimelightAiming {
   private static NetworkTable table = NetworkTableInstance.getDefault().getTable("");
   private static double lastTurret = 0;
   private static PIDController pidController = new PIDController(0.05, 0, 0);
-  /**
-   * @return april tag heading
-   */
-  public static double getAprilTagHeading() {
-    double xOffset = table.getValue("tx").getDouble();
-    if (xOffset == -1) {
-      return 90;
-    } else {
-      return 90 - xOffset;
-    }
-  }
-  /**
-   * @param turret object
-   * @see Notes april tag pose > 0 is recommended
-   * @return heading for simulated april tag
-   */
-  public static double getAprilTagHeading(Turret turret) {
-    double turretangle = turret.getPosition();
-    // if above 360 in degrees the turret is set as 0 for below data to work
-    if (Units.radiansToDegrees(turret.getPosition()) >= 360) {
-      turret.setTargetPosition(0);
-      turretangle = turret.getPosition();
-    }
-    // if the heading is within 30 degrees away from the opposite of the heading of apriltagpose it
-    // sets turret position to point toward april tag
-    if (Math.abs(
-                -Drive.odometry.getPoseMeters().getRotation().getDegrees()
-                    - AprilTagPose.getRotation().getDegrees())
-            < 30
-        && Drive.odometry.getPoseMeters().getY() > AprilTagPose.getX()) {
-      if (lastTurret == turretangle) {
-        return -(turretangle - (1.571 / 400));
-      } else {
-        return turretangle;
-      }
-      // if facing the same heading as the april tag: turn 180
-    } else if (Math.abs(
-                Drive.odometry.getPoseMeters().getRotation().getDegrees()
-                    - AprilTagPose.getRotation().getDegrees())
-            <= 30
-        && Drive.odometry.getPoseMeters().getY() > AprilTagPose.getX() // rotated so different
-        && Math.abs(turretangle - -Drive.odometry.getPoseMeters().getRotation().getRadians())
-            <= 30) {
-      if (lastTurret == turretangle) {
-        lastTurret = turretangle;
-        return turretangle - Units.degreesToRadians(180);
-      } else {
-        lastTurret = turretangle;
-        return (turretangle);
-      }
-    }
-    if (Math.abs(
-                -Drive.odometry.getPoseMeters().getRotation().getDegrees()
-                    - AprilTagPose.getRotation().getDegrees())
-            >= 180
-        && Drive.odometry.getPoseMeters().getY() < AprilTagPose.getX() // rotated so different
-    ) {
-      lastTurret = turretangle;
-      return turretangle - Units.degreesToRadians(180);
-    } else if (Drive.odometry.getPoseMeters().getX() > AprilTagPose.getY()
-        && Math.abs(
-                Drive.odometry.getPoseMeters().getRotation().getDegrees()
-                    - AprilTagPose.getRotation().getDegrees())
-            < 30) {
-      lastTurret = turretangle;
-      return turretangle - Units.degreesToRadians(90);
-    } else {
-      return turretangle + Units.degreesToRadians(-90);
-    }
-  }
 
   /**
-   * @return distance to target
+   * @return distance to target in inches
    */
   public double getDistanceToTarget() {
     double ty = table.getEntry("ty").getDouble(0.0);
